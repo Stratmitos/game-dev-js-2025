b8e69d98baf183867089b8fefc9a8e766f751e3d
Stratmitos <faisalwinarto@gmail.com>
2025-04-16 19:50:18 +0700

    Add: Finishing animation attack & movement

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -1,14 +1,38 @@
 extends Node2D
 
 var mspd: float = 100
+var max_attack_cooldown: float = 2.0
+var min_attack_cooldown: float = 1.3
+
+@onready var state_machine = $AnimationTree["parameters/playback"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	spawn()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
-	position.x += delta * mspd
+	if state_machine.get_current_node() == "movement":
+		if position.x >= 1000.0:
+			attack()
+		else:
+			position.x += delta * mspd
+			state_machine.travel("movement")
 
-func spawn(mspd: float = 100) -> void:
+func spawn(_mspd: float = 100) -> void:
+	state_machine.travel("idle")
+	move()
+
+func move() -> void:
+	state_machine.travel("movement")
+
+func attack() -> void:
+	state_machine.travel("atk-fe")
+	$AttackTimer.wait_time = randf_range(min_attack_cooldown, max_attack_cooldown)
+	$AttackTimer.start()
+
+func _on_animation_tree_animation_finished(_anim_name):
 	pass
+
+func _on_attack_timer_timeout():
+	attack()
