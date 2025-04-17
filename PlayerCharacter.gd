a28dd32c63cc2a65df709c52d7b80cd38b6d6c3f
Stratmitos <faisalwinarto@gmail.com>
2025-04-17 17:39:49 +0700

    Add: Integrate attribute handler
    Note: Todo generated on document game concept

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -1,25 +1,48 @@
 extends Node2D
 
-var mspd: float = 100
-var max_attack_cooldown: float = 2.0
-var min_attack_cooldown: float = 1.3
+var stacked_count: int = randi_range(1, 5)
+var hp: float = 100.0
+var atk: float
+var def: float
+var dominate_rate: float
+var crit_rate: float
+var crit_dmg: float
+var coward_rate: float
+var mspd: float
+var atk_cd: float
+var reckless_rate: float
+var destination:float
+var identity: int
 
 @onready var state_machine = $AnimationTree["parameters/playback"]
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn()
+	$StackedIndicator.text = str(stacked_count)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
 	if state_machine.get_current_node() == "movement":
-		if position.x >= 1000.0:
-			attack()
+		if destination > 250:
+			if position.x <= 1000:
+				position.x += delta * mspd
+			else:
+				state_machine.travel("idle")
 		else:
-			position.x += delta * mspd
-			state_machine.travel("movement")
+			if position.x >= 250:
+				position.x -= delta * mspd
+			else:
+				state_machine.travel("idle")
 
-func spawn(_mspd: float = 100) -> void:
+func spawn(dest: float, id: int) -> void:
+	identity = id
+	destination = dest
+	if destination < 250:
+		$Skin.scale.x = -$Skin.scale.x
+	
+	AttributeHandler.strength.set_value(id, randi_range(10, 100))
+	AttributeHandler.intelligence.set_value(id, randi_range(10, 100))
+	AttributeHandler.agility.set_value(id, randi_range(10, 100))
+	_init_attribute_point_effect()
+
 	state_machine.travel("idle")
 	move()
 
@@ -28,11 +51,55 @@ func move() -> void:
 
 func attack() -> void:
 	state_machine.travel("atk-fe")
-	$AttackTimer.wait_time = randf_range(min_attack_cooldown, max_attack_cooldown)
+	$AttackTimer.wait_time = atk_cd
 	$AttackTimer.start()
+
+func on_character_receive_damage(value: float, node_source: Node2D) -> void:
+	$DamageIndicator.show_damage(value)
+	while value > 0:
+		var current_hp: float = hp
+		hp -= value
+		value -= current_hp
+		if hp <= 0:
+			stacked_count -= 1
+			if stacked_count <= 0:
+				$StackedIndicator.text = str(0)
+				node_source.on_character_kill_enemy()
+			else:
+				$StackedIndicator.text = str(stacked_count)
+				hp = 100.0
+
+		await get_tree().create_timer(0.1).timeout
+
+func on_character_kill_enemy() -> void:
+	$AttackTimer.stop()
+	move()
+
+func _init_attribute_point_effect() -> void:
+	atk = AttributeHandler.strength.get_atk_point(identity) * stacked_count
+	def = AttributeHandler.strength.get_def_point(identity) * stacked_count
+	dominate_rate = AttributeHandler.get_dominate_chance(identity)
+
+	crit_rate = AttributeHandler.intelligence.get_critical_rate(identity)
+	crit_dmg = AttributeHandler.intelligence.get_critical_damage(identity)
+	coward_rate = AttributeHandler.get_coward_chance(identity)
+
+	mspd = 100 + AttributeHandler.agility.get_movement_speed(identity)
+	atk_cd = 1.7 - AttributeHandler.agility.get_attack_speed(identity)
+	reckless_rate = AttributeHandler.get_reckless_chance(identity)
 
 func _on_animation_tree_animation_finished(_anim_name):
 	pass
 
 func _on_attack_timer_timeout():
 	attack()
+
+func _on_attack_range_detect_enemy(_area):
+	attack()
+
+func _on_apply_attack_detect_enemy(area):
+	area.get_parent().get_parent().on_character_receive_damage(atk, self)
+
+func _on_indicator_damage_hide():
+	if hp <= 0:
+		queue_free()
