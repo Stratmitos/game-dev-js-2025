8ebc6df52bc0b4c690df16026ed4f7a671f157cf
Stratmitos <faisalwinarto@gmail.com>
2025-04-20 23:33:56 +0700

    Add: core loop gameplay

--- Gameplay.gd
+++ Gameplay.gd
@@ -1,16 +1,30 @@
 extends Node2D
 
+@onready var ui_node: Panel = $MenuPanel
+
+@onready var economy: Panel = ui_node.get_node("Economy")
+
+@onready var statistic: Panel = ui_node.get_node("StatisticPanel")
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
+	start()
+	$GirlTroop.war_is_over.connect(func(is_win) : _on_war_is_over(is_win))
+	$Slime.war_is_over.connect(func(is_win) : _on_war_is_over(is_win))
+
+func start() -> void:
 	$GirlTroop.spawn(1052.0, AttributeHandler.player)
 	$Slime.spawn(100.0, AttributeHandler.enemy)
-	$MenuPanel/StatisticPanel.refresh_attribute()
+	economy.set_day_passed(economy.get_day_passed() + 1)
+	statistic.refresh_attribute()
+	print("Start over")
 
 func _on_player_recruit_troop(value: int) -> void:
 	$GirlTroop.on_player_add_troop(value)
 
-func _on_player_start_war(event):
+func _on_player_start_war(event) -> void:
 	if event is InputEventMouseButton and event.pressed:
+		MoneyHandler.toggle_allowed_to_spent_money(false)
 		$GirlTroop.move()
 		$Slime.move()
 
