54e1251b3cfa9d42d676a8751fe42b07258bdff8
Stratmitos <faisalwinarto@gmail.com>
2025-04-25 11:09:46 +0700

    Add: Integrate back to main menu

--- services/UI/FinalResult.gd
+++ services/UI/FinalResult.gd
@@ -2,4 +2,8 @@ extends Panel
 
 func showup(text_val: String) -> void:
 	visible = true
-	$Label.text = text_val
+	$Label.text = text_val + "\nPress Enter / Space to back to main menu"
+
+func _input(event) -> void:
+	if event.is_pressed() and InputMap.event_is_action(event, "ui_accept") and visible:
+		SceneManager.goto_scene("uid://dcpwpugdsfu81")
